8cff5a1527c2a2e4205c254316bd2a8ec4a2d171
Shiina <417keikunv2@gmail.com>
2025-12-14 19:53:09 +0900

    全ノードで出力されるログレベルをwarn以上に変更

--- launch/manual_operation.launch.py
+++ launch/manual_operation.launch.py
@@ -5,26 +5,33 @@ def generate_launch_description():
     controller_receiver_node = Node(
         package='joy',
         executable='joy_node',
-        name='controller_receiver'
+        name='controller_receiver',
+        arguments=['--ros-args', '--log-level', 'warn'],
     )
 
     cmd_vel_pad_node = Node(
         package='imrc_pad_cmd_vel',
         executable='pad_cmd_vel',
-        name='joy_to_cmd_vel'
+        name='joy_to_cmd_vel',
+        arguments=['--ros-args', '--log-level', 'warn'],
+
     )
 
     cmd_vel_selector_node = Node(
         package='imrc_cmd_vel_selector',
         executable='cmd_vel_selector',
-        name='cmd_vel_selector'
+        name='cmd_vel_selector',
+        arguments=['--ros-args', '--log-level', 'warn'],
+
     )
 
     uart_bridge_node = Node(
         package='uart_bridge',
         executable='bridge',
         name='uart_bridge',
-        output='screen'
+        output='screen',
+        arguments=['--ros-args', '--log-level', 'warn'],
+
     )
 
     return LaunchDescription([
